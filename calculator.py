1ca25ec05aad0272b5daaa5346e11767ceff59c6
SnehavKhaniya <snehakhaniya18@gmail.com>
2024-07-31 00:20:14 +0530

    production and test separated

--- calculator.py
+++ calculator.py
@@ -1,43 +1,35 @@
 import re
-
 class NegativeNumberException(Exception):
-    def __init__(self, negative_numbers):
-        self.negative_numbers = negative_numbers
+    def __init__(self, negative_num_list):
+        self.negative_num_list = negative_num_list
+        negative_numbers = ' '.join(self.negative_num_list)
         super().__init__(f"negative numbers are not allowed {negative_numbers}")
 
-def check_for_negative_numbers(numbers):
-    negative_pattern = r'-\d+'
-    negative_nums = re.findall(negative_pattern, numbers)
+def check_for_negative_numbers(input_str):
+    negative_num_pattern = r'-\d+'
+    negative_num_list = re.findall(negative_num_pattern, input_str)
     
-    if len(negative_nums) != 0:
-        raise NegativeNumberException(negative_nums)
+    if len(negative_num_list) != 0:
+        raise NegativeNumberException(negative_num_list)
 
-def add(numbers = 0):
-    try:
-        if numbers == 0 or numbers == "":
-            return 0
-        
-        if numbers.isdigit():
-            return int(numbers)
-        
-        check_for_negative_numbers(numbers)
-        
-        if numbers[0:2] != "//":
-            nums = []
-            list_items = re.split(r'[,\n]', numbers)
-            for item in list_items:
-                nums.append(item)
-            
-        else:
-            delimiter = numbers[2]
-            numbers = numbers[4:]
-            nums = re.split(delimiter, numbers)
-        
-        _sum = 0
-        for i in nums:
-            _sum += int(i)
-        return _sum
-    except NegativeNumberException as e:
-        print(f"negative numbers are not allowed {e.negative_numbers}")
-        raise
-add("-1")
+def add(input_str = "0"):
+    
+    if input_str.isdigit():
+        return int(input_str)
+    
+    if input_str == "":
+        return 0
+    
+    check_for_negative_numbers(input_str)
+    
+    if input_str[0:2] != "//":
+        num_list = re.split(r'[,\n]', input_str)
+    else:
+        delimiter = input_str[2]
+        input_str = input_str[4:]
+        num_list = re.split(delimiter, input_str)
+    
+    _sum = 0
+    for num in num_list:
+        _sum += int(num)
+    return _sum
--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 import re
-
+from calculator import add, NegativeNumberException
 class Test(unittest.TestCase):
      
     def test_add_return_sum(self):
@@ -29,40 +29,3 @@ class Test(unittest.TestCase):
         
 if __name__ == '__main__':
     unittest.main()
-
-
-class NegativeNumberException(Exception):
-    def __init__(self, negative_num_list):
-        self.negative_num_list = negative_num_list
-        negative_numbers = ' '.join(self.negative_num_list)
-        super().__init__(f"negative numbers are not allowed {negative_numbers}")
-
-def check_for_negative_numbers(input_str):
-    negative_num_pattern = r'-\d+'
-    negative_num_list = re.findall(negative_num_pattern, input_str)
-    
-    if len(negative_num_list) != 0:
-        raise NegativeNumberException(negative_num_list)
-
-def add(input_str = "0"):
-    
-    if input_str.isdigit():
-        return int(input_str)
-    
-    if input_str == "":
-        return 0
-    
-    check_for_negative_numbers(input_str)
-    
-    if input_str[0:2] != "//":
-        num_list = re.split(r'[,\n]', input_str)
-    else:
-        delimiter = input_str[2]
-        input_str = input_str[4:]
-        num_list = re.split(delimiter, input_str)
-    
-    _sum = 0
-    for num in num_list:
-        _sum += int(num)
-    return _sum
-    
