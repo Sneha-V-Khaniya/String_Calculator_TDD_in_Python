ff1644059d673680a0db30f45adcaac4c3779f31
SnehavKhaniya <snehakhaniya18@gmail.com>
2024-07-30 18:53:16 +0530

    setup done

---
+++ test_calculator.py
@@ -0,0 +1,12 @@
+import unittest
+
+class Test(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(0, 0)
+        
+if __name__ == '__main__':
+    unittest.main()
+
+
+def add(numbers):
+    pass
