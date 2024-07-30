78436c28c9f4c6647a424e58a540be3ba2c3bb21
SnehavKhaniya <snehakhaniya18@gmail.com>
2024-07-30 19:40:20 +0530

    sum can handle , and \n delimiters

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-
+import re
 class Test(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(), 0)
@@ -7,6 +7,12 @@ class Test(unittest.TestCase):
         self.assertEqual(add("1"), 1)
         self.assertEqual(add("234"), 234)
         self.assertEqual(add("1,3"), 4)
+        self.assertEqual(add("2, 4"), 6)
+        self.assertEqual(add("1,2,3,4,5"), 15)
+        self.assertEqual(add("1, 2, 3, 4, 5"), 15)
+        self.assertEqual(add("1\n2"), 3)
+        self.assertEqual(add("1,2\n3"), 6)
+        self.assertEqual(add("1\n2,3, 4\n 5"), 15)
         
 
 if __name__ == '__main__':
@@ -19,9 +25,10 @@ def add(numbers = 0):
     if numbers.isdigit():
         return int(numbers)
     else:
-        nums = numbers.split(",")
+        nums = re.split(r'[,\n]', numbers)
         _sum = 0
         for i in nums:
             _sum += int(i)
         return _sum
-        
+    
+    
