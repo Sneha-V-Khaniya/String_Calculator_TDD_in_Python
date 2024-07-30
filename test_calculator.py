d8f74db622c798ff2498591993c6f6729af73e81
SnehavKhaniya <snehakhaniya18@gmail.com>
2024-07-30 19:19:53 +0530

    base testcase passed

--- test_calculator.py
+++ test_calculator.py
@@ -2,11 +2,26 @@ import unittest
 
 class Test(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(0, 0)
+        self.assertEqual(add(), 0)
+        self.assertEqual(add(""), 0)
+        self.assertEqual(add("1"), 1)
+        self.assertEqual(add("234"), 234)
+        self.assertEqual(add("1,3"), 4)
         
+
 if __name__ == '__main__':
     unittest.main()
 
-
-def add(numbers):
-    pass
+def add(numbers = 0):
+    if numbers == 0 or numbers == "":
+        return 0
+    
+    if numbers.isdigit():
+        return int(numbers)
+    else:
+        nums = numbers.split(",")
+        _sum = 0
+        for i in nums:
+            _sum += int(i)
+        return _sum
+        
